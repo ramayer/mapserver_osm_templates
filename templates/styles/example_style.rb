edf86ed6df902793909f17ea20a2caeaa2638e8c
Ron Mayer <ramayer+git@gmail.com>
2015-05-26 16:37:38 -0700

    userfeatures script from the old 'rmap' app

---
+++ templates/styles/example_style.rb
@@ -0,0 +1,68 @@
+$map_options1  = {
+       road_color_1:		 '#888888',       # major highways
+       road_color_2:		 '#999999', 	  # oregon expressway, etc
+       road_color_3:		 '#aaaaaa',  	  # el camino, alma, hwy 9, etc.
+       road_color_4:		 '#bbbbbb', 	  # the inside of smaller roads - Stevenson and all smalll roads.
+       road_color_5:		 '#cccccc',   	  # roads zoomed out so they're just 1px wide
+       oneway_color:		 '#666666',   	  # the arrows on one-way roads
+       water_color:		 '#aaaabb', 	  # water
+       forest_color:		 '#eeffee',  	  # forest
+       grass_color:		 '#eeffee',  	  # grass
+       land_color_1:		 '#ffffff',   	  # basic polygons
+       industrial_color:	 '#eeeeee',    	  # airport, military, railways, brownfield, commercial, cemetary parking
+       school_color:		 '#cc8888',       # school, college, university
+       land_outline_color_1:	 '#aaaaaa', 	  # state outline
+       residential_color:	 '#ffffff',       # 'residential'
+}
+
+$map_options2  = {
+       road_color_1:		 '#888888',       # major highways
+       road_color_2:		 '#999999', 	  # oregon expressway, etc
+       road_color_3:		 '#aaaaaa',  	  # el camino, alma, hwy 9, etc.
+       road_color_4:		 '#bbbbbb', 	  # the inside of smaller roads - Stevenson and all smalll roads.
+       road_color_5:		 '#cccccc',   	  # roads zoomed out so they're just 1px wide
+       oneway_color:		 '#666666',   	  # the arrows on one-way roads
+
+       water_color:		 '#111111', 	  # water
+       forest_color:		 '#001100',  	  # forest
+       grass_color:		 '#001100',  	  # grass
+       land_color_1:		 '#000000',   	  # basic polygons
+       industrial_color:	 '#222222',    	  # airport, military, railways, brownfield, commercial, cemetary parking
+       school_color:		 '#221111',       # school, college, university
+       land_outline_color_1:	 '#444444', 	  # state outline
+       residential_color:	 '#000000',       # 'residential'
+       park_color:               '#B5D29C',       # golf course, park, recreation_ground, cemetary (other zoom)
+}
+
+
+$map_options_debug  = {
+       road_color_1:		 '#000000',       # major highways
+       road_color_2:		 '#000000', 	  # oregon expressway, etc
+       road_color_3:		 '#000000',  	  # el camino, alma, hwy 9, etc.
+       road_color_4:		 '#000000', 	  # the inside of smaller roads - Stevenson and all smalll roads.
+       road_color_5:		 '#000000',   	  # roads zoomed out so they're just 1px wide
+       oneway_color:		 '#000000',   	  # the arrows on one-way roads
+
+       water_color:		 '#000000', 	  # water
+       forest_color:		 '#000000',  	  # forest
+       grass_color:		 '#000000',  	  # grass
+       land_color_1:		 '#000000',   	  # basic polygons
+       industrial_color:	 '#000000',    	  # airport, military, railways, brownfield, commercial, cemetary parking
+       school_color:		 '#000000',       # school, college, university
+       land_outline_color_1:	 '#000000', 	  # state outline
+       residential_color:	 '#000000',       # 'residential'
+       park_color:               '#000000',       # golf course, park, recreation_ground, cemetary (other zoom)
+       hospital_color:           '#000000',          # hospital
+
+       rail_color:               '#ff0000',          # 
+       rail_outline_color:       '#00ff00',          # 
+       rail_symbol_color:        '#0000ff',          # 
+
+}
+
+
+$test_map_options  = {
+       school_color:		 '#ff0000',
+}
+
+$map_options = $map_options2
